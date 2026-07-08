189ba451ffe2d93a2ec1499f95251235c2780247
murtazaarshad1805-lab <murtazaarshad1805@gmail.com>
2026-07-08 17:28:55 +0500

    Numbers

---
+++ 01_basics/06_numbers_and_math.js
@@ -0,0 +1,13 @@
+const score = 400;
+//console.log(score);
+
+const num = new Number(100);
+// console.log(num);
+// console.log(num.toString());
+// console.log(num.toString().length);
+// console.log(num.toFixed(2)); 
+// console.log(num.toFixed(3)); 
+
+
+const balance = 323.9867
+console.log(balance.toPrecision(4)); 
