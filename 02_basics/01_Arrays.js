193ecbe2c832b09be45ac05bb146df6a645e4c25
murtazaarshad1805-lab <murtazaarshad1805@gmail.com>
2026-07-09 17:17:17 +0500

    Arrays  study

---
+++ 02_basics/01_Arrays.js
@@ -0,0 +1,11 @@
+const myArr = [1, 2, 3, 4, 5]
+console.log(myArr[7]);
+
+const myHeros = ["SuperMen", "SpiderMen", "IronMen", "Spider Noir", "Captain America"]
+console.log(myHeros[4]);
+
+const numbers = [3, 5, 6, 7, 2, true, "Ali Raza"]
+console.log(numbers[3]);
+
+const myArray = new Array(4, 5, 564, 6, 46, 87, 7)
+console.log(myArray.length);
