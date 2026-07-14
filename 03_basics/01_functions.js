47b21d51c19fc2ac207eab3e72bcd3f7c4920e23
murtazaarshad1805-lab <murtazaarshad1805@gmail.com>
2026-07-14 17:09:56 +0500

    Function in Javascript

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,39 @@
+function myName() {
+    console.log("Muhammad");
+    console.log("Murtaza");
+    console.log("Arshad");
+}
+
+// myName                                                   // Function Reference
+// myName()                                                 // Function Exicution
+
+
+// function addTwoNumber(num1, num2) {                         // (num1, num2) => Is called Parameter 
+//     console.log(num1 + num2);
+// }
+
+
+function addTwoNumber(num1, num2) {                         // (num1, num2) => Is called Parameter 
+    // let result = num1 + num2
+    // return result
+    return num1 + num2
+} 
+
+const result = addTwoNumber(3,97)                                           // (3,4) => Is called Arguments
+ console.log("Result: " , result);
+
+
+
+function loginUserMessage(username) {
+    if (!username) {
+        return console.log("Please enter a Username.");
+    } 
+    return `${username} is loged In`;
+}
+console.log(loginUserMessage("Murtaza"));                               // Print: Murtaza is loged In
+console.log(loginUserMessage(""));                                      // Print:   is loged In
+console.log(loginUserMessage());                                        // Print: undefined is loged In
+
+
+
+
