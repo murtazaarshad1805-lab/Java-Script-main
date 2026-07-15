dd2bb80ebc071eb487564fdbffb2664857520481
murtazaarshad1805-lab <murtazaarshad1805@gmail.com>
2026-07-15 12:34:36 +0500

    Scope in Javascript

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -2,10 +2,9 @@
     {
         This is a BLOCK SCOPE in curly brasis
         Out side Function is called GLOBAL SCOPE
+        DOM Stand For: Document Object Modal
     }
 */
-
-const { forwardRef } = require("react");
 
 let a = 100
 
@@ -16,4 +15,60 @@ if (true) {
 }
 
 console.log("Outer: ", a);
+
+
+
+
+Closure 
+
+function one(){
+    const userName = "Murtaza"
+
+    function two() {
+        let userSubject = "Java Script"
+        console.log(userName);
+    }
+
+    // console.log(userSubject);
+    two()
+
+}
+
+one()
+
+if (true) {
+    const userName = "Ali"
+
+    if (userName == "Ali") {
+        const secondName = " Raza"
+        console.log(userName , secondName);
+    }
+    console.log(secondName);
+
+}
+
+console.log(userName);
+
+
+
+
+// ************************** Intresting ++++++++++++++++++++++++++++++++++++
+
+
+
+console.log(addOne(4));                   // This line Execute
+function addOne(num) {
+    return num + 1
+}
+
+console.log(addOne(4));
+
+// console.log(addTwo(5));                 Error: Cannot access 'addTwo' before initialization
+const addTwo = function(num) {
+    return num + 2
+}
+
+console.log(addTwo(5));
+
+
 
