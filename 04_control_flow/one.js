dbd292af8ba3294965271e17fbfd9f7fae2d177a
murtazaarshad1805-lab <murtazaarshad1805@gmail.com>
2026-07-17 16:40:43 +0500

    If Statement in JS

---
+++ 04_control_flow/one.js
@@ -0,0 +1,69 @@
+// If Statement
+
+/*
+
+1. Greater Then                                (>)         |        5. Not Equal                                   (!=)
+2. Less Then                                   (<)         |        6. Strict Not Equal                            (!==)
+3. Greater Then or Equal                       (>=)        |        7. Equal (only value)                          (==)
+4. Less Then or Qeual                          (<=)        |        8. Strict Equal (value or type)                (===)
+
+*/
+
+// const temprature = 44
+
+// if (temprature !== 50) {
+//     console.log("tempreture less then 50");
+// } else {
+//     console.log("Temprature graterthen 50");
+// }
+
+// console.log("Always Executed");
+
+
+// const score = 200
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+
+
+// Short Hand Notation
+
+// const balance = 1000
+
+// if (balance > 500) console.log("Execute and Best Way");
+
+// if (balance > 500) console.log("Execute"), console.log("But Not Best Way");
+
+// if (balance < 500) {
+//     console.log("Less then 500");
+// } else if (balance < 750) {
+//         console.log("Less then 750");
+// } else if (balance < 900) {
+//         console.log("Less then 900");
+// } else {
+//         console.log("Less then 1200");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const userLogedInGoogle = false
+const userLogedInEmail = true
+
+if (userLoggedIn && debitCard) {
+    console.log("Allow to buy a Cource");
+}
+
+if (userLogedInGoogle || userLogedInEmail) {
+    console.log("user Login");
+}
+
+
+
+
+
+
+
+
