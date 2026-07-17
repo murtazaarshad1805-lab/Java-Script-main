6e96c7c4214248c0152bd455fa9e1aa9481fce67
murtazaarshad1805-lab <murtazaarshad1805@gmail.com>
2026-07-17 16:57:19 +0500

    Switch Stement in JS

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,62 @@
+// Switch Stement
+
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+// const month = 3;
+
+// switch (month) {
+//   case 1:
+//     console.log("January");
+//     break;
+//   case 2:
+//     console.log("February");
+//     break;
+//   case 3:
+//     console.log("March");
+//     break;
+//   case 4:
+//     console.log("April");
+//     break;
+//   case 5:
+//     console.log("May");
+//     break;
+
+//   default:
+//     console.log("Default Value");
+//     break;
+// }
+
+
+
+const month = "May";
+
+switch (month) {
+  case "January":
+    console.log("January");
+    break;
+  case "February":
+    console.log("February");
+    break;
+  case "March":
+    console.log("March");
+    break;
+  case "April":
+    console.log("April");
+    break;
+  case "May":
+    console.log("May");
+    break;
+
+  default:
+    console.log("Default Value");
+    break;
+}
