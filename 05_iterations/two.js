318b8599c1febe11aec4dd7fe08a03aca66e1534
murtazaarshad1805-lab <murtazaarshad1805@gmail.com>
2026-07-20 12:32:54 +0500

    While or Do While Loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,36 @@
+// While Loop **************************
+
+
+let num = 0
+
+while (num <= 10) {
+    // console.log(`Value of Number is: ${num}`);
+    num = num + 2
+}
+
+
+const myArray = [1, "Super Man", 2, "Spider Man", 3, "Iron Man", 4, "Ant Man"]
+// console.log(`Array Length : ${myArray.length}`);
+
+let num1 = 0
+
+while (num1 < myArray.length) {
+    // console.log(`Number is: ${myArray[num1]}`);
+    num1 = num1 + 1
+}
+
+
+
+
+// Do While Loop ***************************
+
+let score = 1
+do {
+    console.log(`Score is: ${score}`);
+    score++
+} while (score <= 10);
+
+
+
+
+
