6ac876352d92a36e492130c959cda285a0117482
murtazaarshad1805-lab <murtazaarshad1805@gmail.com>
2026-07-20 11:02:34 +0500

    Truthy, Falsy, nullish and terniory

--- 04_control_flow/truthy_falsy.js
+++ 04_control_flow/truthy_falsy.js
@@ -1,12 +1,65 @@
-const userEmail = "ali@123gmail.com"
+// const userEmail = "ali@123gmail.com"
 
-if (userEmail) {
-    console.log("Got the user Email.");
-} else {
-    console.log("Dont have a user email.");
-}
+// if (userEmail) {
+//     console.log("Got the user Email.");
+// } else {
+//     console.log("Dont have a user email.");
+// }
 
 
 // Falsy Values:        ===>        false , 0 , -0 , BigInt 0n , "" ,  null , undefined , NaN
 
 // Truthy Values:       ===>        "0" , 'false' , " " , {} , [] , function(){}
+
+
+
+// Array is epmty Check using IF condition
+
+// const userDetail = []
+// if (userDetail.length === 0) {
+//     console.log("Array is epmty.");
+// }
+
+
+
+// Object is epmty check using IF condition
+
+// const emptyObject = {}
+// if (Object.keys(emptyObject).length === 0) {
+//     console.log("Object is Epmty.");
+// }
+
+
+
+
+
+// Nullish coalescing operator (??)   check ===> null , undefined
+
+// let val;
+
+// val = 5 ?? 10                        // Answer is :=> 5
+// val = null ?? 15                     // Answer is :=> 15
+// val = undefined ?? 20                // Answer is :=> 20
+// val = null ?? 30 ?? 40               // Answer is :=> 30
+
+// console.log(val);
+
+
+
+
+
+// Terniary Operator 
+
+// Condition ? true : false             // Syntax
+
+const iceTeaPrice = 100
+
+iceTeaPrice <= 80 ? console.log("Less then 80.") : console.log("More then 80.")
+
+
+
+
+
+
+
+
